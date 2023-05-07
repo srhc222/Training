bffeb7e3e98d37f3bfd37ccd23e00735348b5a28
srhc222 <srhc222@gmail.com>
2023-05-07 13:49:28 +0300

    3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { Test2Component } from './test2/test2.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { ContentComponent } from './layout/content/content.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    Test2Component
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
