f71180af95e9978818a5756c88264ec6ca59e27a
srhc222 <srhc222@gmail.com>
2023-05-07 13:19:58 +0300

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
