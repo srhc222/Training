90ebbfc5253a6728bb5828cf2b7f349668d07206
srhc222 <srhc222@gmail.com>
2023-05-23 17:39:23 +0300

    1

---
+++ src/app/data/pages-data.ts
@@ -0,0 +1,22 @@
+import { Page } from '../model/page';
+
+export const Catalog: Page = {
+  title: 'Catalog',
+  isMain: true,
+  pageNumber: 1,
+  imagePagePath: '',
+};
+
+export const Home: Page = {
+  title: 'Home',
+  isMain: true,
+  pageNumber: 2,
+  imagePagePath: '',
+};
+
+export const ItemDetails: Page = {
+  title: 'Item Details',
+  isMain: true,
+  pageNumber: 3,
+  imagePagePath: '',
+};
